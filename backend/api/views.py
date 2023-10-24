f3025d273a3d08db1572fdd9d286df9b7af23150
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-10-24 09:08:01 +0000

    added PublicUserViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,27 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+    AllowAny
+)
 
-from students.models import StudentResume
 
-from .serializers import StudentUserSerializer
+from djoser.views import UserViewSet as DjoserUserViewSet
+
+from .serializers import StudentUserSerializer, UserSerializer
+from students.models import StudentResume, User
 
 
 class StudentUserViewSet(viewsets.ModelViewSet):
     queryset = StudentResume.objects.all()
     serializer_class = StudentUserSerializer
+
+
+class PublicUserViewSet(DjoserUserViewSet):
+    """Представление для работы с публичными данными пользователей."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    pagination_class = PageNumberPagination
+    # permission_classes = [IsAuthenticated]
