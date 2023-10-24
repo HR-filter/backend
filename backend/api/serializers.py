4612aaf156542bb43d0a0d49ea2f4639d44a0c59
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-10-24 09:07:25 +0000

    added UserCreateSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,59 @@
 from rest_framework import serializers
-from students.models import StudentResume, ContactInfo, StudentPosition, Skill
+from students.models import StudentResume, ContactInfo, StudentPosition, Skill, User
 from drf_extra_fields.fields import Base64ImageField
+
+from djoser.serializers import UserCreateSerializer
+
+
+class UserSerializer(UserCreateSerializer):
+    """Сериализатор для модели пользователя."""
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "password",
+        ]
+
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def create(self, validated_data):
+        """Создаёт нового пользователя с запрошенными полями.
+
+        Args:
+            validated_data (dict): Полученные проверенные данные.
+
+        Returns:
+            User: Созданный пользователь.
+        """
+        user = User(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
+
+    def to_representation(self, instance):
+        """
+        Преобразует объект пользователя в представление JSON.
+        """
+        data = super().to_representation(instance)
+        request = self.context.get("request")
+
+        if request.method == "GET":
+            data.pop("password", None)
+
+        return data
 
 
 class StudentPositionSerializer(serializers.ModelSerializer):
@@ -9,9 +62,7 @@ class StudentPositionSerializer(serializers.ModelSerializer):
     и академического статуса.
     """
 
-    position_name = serializers.StringRelatedField(
-        source="position", read_only=True
-    )
+    position_name = serializers.StringRelatedField(source="position", read_only=True)
     academic_status_name = serializers.StringRelatedField(
         source="academic_status", read_only=True
     )
@@ -61,9 +112,7 @@ class StudentUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(source="user.email")
     contact_info = ContactInfoSerializer()
     photo = Base64ImageField()
-    training_status = StudentPositionSerializer(
-        many=True, source="student_positions"
-    )
+    training_status = StudentPositionSerializer(many=True, source="student_positions")
     academic_status = serializers.StringRelatedField()
     employment_status = serializers.StringRelatedField()
     skills = SkillSerializer(many=True)
