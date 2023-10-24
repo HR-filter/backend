15a1cf678de6bf8aea455011229468dd0b29a59a
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-10-24 09:07:47 +0000

    added path/users

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,18 +2,19 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from .views import StudentUserViewSet
+from .views import StudentUserViewSet, PublicUserViewSet
 from docs.views import schema_view
 
 app_name = "api"
 
 # Роутер для API версии 1
 router_v1 = routers.DefaultRouter()
-router_v1.register(r"test", StudentUserViewSet, "users")
+router_v1.register(r"test", StudentUserViewSet, "test")
+router_v1.register("users", PublicUserViewSet, "users")
 
 urlpatterns = (
     path("", include(router_v1.urls)),
-    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
     path(
         "redoc/",
         schema_view.with_ui("redoc", cache_timeout=0),
